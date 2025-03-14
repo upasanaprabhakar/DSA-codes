873c556ead404d678014ae9a1ee630bd64b52f95
UPASANA <upasanaprabhakar35@gmail.com>
2025-03-14 21:29:11 +0530

    Leetcode-89

---
+++ linked_list.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    public:
+        ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+         ListNode* c = new ListNode();
+         ListNode* temp = c;
+         int carry = 0;
+         while(l1!=NULL || l2!=NULL || carry){
+            int sum = 0;
+            if(l1!=NULL){
+                sum+=l1->val;
+                l1=l1->next;
+            }
+            if(l2!=NULL){
+                sum+=l2->val;
+                l2 = l2->next;
+            }
+            sum = sum+carry;
+            carry = sum/10;
+            ListNode* d = new ListNode(sum%10);
+            temp->next = d;
+            temp=d; 
+         }  
+         return c->next; 
+        }
+    };
