2379328771296b3a030ef84867199906c1be6fe2
UPASANA <upasanaprabhakar35@gmail.com>
2025-03-18 22:01:20 +0530

    Leectcode-1710

---
+++ maximum_units_truck.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    public:
+        int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+            
+            auto cmp = [](vector<int>& a, vector<int>& b) {
+                return a[1] > b[1];
+            };
+    
+            sort(boxTypes.begin(), boxTypes.end(), cmp);
+            
+            int profit = 0;
+            for (int i = 0; i < boxTypes.size(); i++) {
+                if (boxTypes[i][0] <= truckSize) {
+                    profit += boxTypes[i][0] * boxTypes[i][1];
+                    truckSize -= boxTypes[i][0];
+                } else {
+                    profit += truckSize * boxTypes[i][1];
+                    truckSize = 0;
+                }
+                if (truckSize == 0) break;
+            }
+            return profit;
+        }
+    };
+    
