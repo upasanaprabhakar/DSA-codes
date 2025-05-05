52d87ff3420bac4a120d1172658b19ba7b7efe10
UPASANA <upasanaprabhakar35@gmail.com>
2025-05-05 19:02:04 +0530

    Prim's MST

---
+++ prims_mst.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define pp pair<int,int>
+using namespace std;
+
+vector<list<pp> >gr;
+void add_edge(int u, int v, int wt, bool bi_dir=true){
+    gr[u].push_back({v,wt});
+    if(bi_dir){
+        gr[v].push_back({u,wt});
+    }
+}
+
+ll prims(int src, int n){
+    priority_queue<pp, vector<pp>, greater<pp> >pq; //{wt,node}
+    unordered_set<int> vis;
+    vector<int> par(n+1);
+    unordered_map<int,int> mp;
+    for(int i=1;i<=n;i++){
+        mp[i]= INT_MAX;
+    }
+
+    pq.push({0,src});
+    mp[src]= 0;
+    int total_count= 0;
+    int result= 0;  //sum of wts
+    while(total_count < n && !pq.empty()){
+        pp curr= pq.top();
+        if(vis.count(curr.second)){
+            pq.pop();
+            continue;
+        }
+        vis.insert(curr.second);
+        total_count++;
+        result+= curr.first;
+        pq.pop();
+
+        for(auto neighbour : gr[curr.second]){
+            if(!vis.count(neighbour.first) && mp[neighbour.first] > neighbour.second){
+                pq.push({neighbour.second, neighbour.first});
+                par[neighbour.first]= curr.second;
+                mp[neighbour.first]= neighbour.second;
+            }
+        }
+    }
+    return result;
+}
+
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    gr.resize(n+1, list<pp> ());
+    while(m--){
+        int u,v,wt;
+        cin>>u>>v>>wt;
+        add_edge(u,v,wt);
+    }
+    int src;
+    cin>>src;
+    cout<<prims(src,n)<<"\n";
+    return 0;
+}
+/*
+Example Test Case:
+4 5
+1 2 1
+1 3 4
+2 3 2
+2 4 6
+3 4 3
+1
+6 -> Output
+*/
