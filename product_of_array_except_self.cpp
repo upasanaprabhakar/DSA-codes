c90cf14315eeae7aa2d2f06742567571a0c6cc9f
UPASANA <upasanaprabhakar35@gmail.com>
2025-05-14 22:14:23 +0530

    Leetcode-238

---
+++ product_of_array_except_self.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> pre(n);
+        vector<int> suf(n);
+
+        int p = nums[0];
+        pre[0] = 1;
+        for (int i = 1; i < n; i++) {
+            pre[i] = p;
+            p *= nums[i];
+        }
+        p = nums[n - 1];
+        suf[n - 1] = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            suf[i] = p;
+            p *= nums[i];
+        }
+        for (int i = 0; i < n; i++) {
+            pre[i] = pre[i] * suf[i];
+        }
+        return pre;
+    }
+};
