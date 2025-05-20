c88c5faac858127810a7db8c1aa84dc9f4738530
UPASANA <upasanaprabhakar35@gmail.com>
2025-05-20 22:51:02 +0530

    Leetcode-3355

---
+++ zero_array_transformation_1.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        int n = nums.size();
+
+        // Step 1: Make diff array using queries
+        vector<int> diff(n, 0);
+        for (auto &query : queries) {
+            int start = query[0];
+            int end = query[1];
+            int x = 1;
+
+            diff[start] += x;
+            if ((end + 1) < n) {
+                diff[end + 1] -= x;
+            }
+        }
+
+        // Step 2: Find cumulative effect on each index
+        vector<int> result(n, 0);
+        int cumSum = 0;
+        for (int i = 0; i < n; i++) {
+            cumSum += diff[i];
+            result[i] = cumSum;
+        }
+
+        // Step 3: Check if result[i] is enough to reduce nums[i] to 0
+        for (int i = 0; i < n; i++) {
+            if (result[i] < nums[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
