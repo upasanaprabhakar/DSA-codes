2bed7c0c41fe1fee3dfeba78ae0d67756a7a9e10
UPASANA <151016634+upasanaprabhakar@users.noreply.github.com>
2025-09-08 22:07:07 +0530

    Leetcode-1317

---
+++ convert_to_two_no_zero_integers.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValid(int num) {
+        while (num > 0) {
+            if (num % 10 == 0)
+                return false;
+            num /= 10;
+        }
+        return true;
+    }
+    vector<int> getNoZeroIntegers(int n) {
+        for (int a = 1; a < n; a++) {
+            int b = n - a;
+            if (isValid(a) && isValid(b)) {
+                return {a, b};
+            }
+        }
+        return {};
+    }
+};
