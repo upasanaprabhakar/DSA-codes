319cd43dff4021f7676af6b1cf574015d7cc0122
UPASANA <151016634+upasanaprabhakar@users.noreply.github.com>
2026-01-04 15:05:50 +0530

    Leetcode-1390

---
+++ four_divisors.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int sumFourDivisors(vector<int>& nums) {
+        int ans = 0;
+        for(int n : nums){
+            int count =0;
+            int sum =0;
+
+            for(int i=1; i*i <= n; i++){
+                if(n%i == 0){
+                    if(i*i == n){
+                        count++;
+                        sum += i;
+                    }
+
+                    else{
+                        count+=2;
+                        sum+= i+(n/i);
+                    }
+                }
+               
+                if(count > 4){
+                    break;
+                }
+            }
+            if(count == 4){
+                ans += sum;
+            }
+        }
+        return ans;
+    }
+};
