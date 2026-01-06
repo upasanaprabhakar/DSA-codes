b9484c01f7e3b0d50f39470b7c5b5bc0a0c7b963
UPASANA <151016634+upasanaprabhakar@users.noreply.github.com>
2026-01-06 15:16:13 +0530

    Leetcode-523

---
+++ continous_subarray_sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        int n= nums.size();
+        unordered_map<int,int> mp;
+
+        mp[0] =-1;
+        int prefixSum = 0;
+        for(int i=0;i<n;i++){
+            prefixSum += nums[i];
+
+            int rem = prefixSum%k;
+
+            if(mp.find(rem) != mp.end()){
+                if(i - mp[rem] >= 2){
+                    return true;
+                }
+            }
+            else{
+                mp[rem] = i;
+            }
+        }
+        return false;
+    }
+};
