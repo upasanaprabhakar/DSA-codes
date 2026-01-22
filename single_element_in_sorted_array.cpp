4244e50e9b158703adab6413e082110a7718403d
UPASANA <151016634+upasanaprabhakar@users.noreply.github.com>
2026-01-22 16:15:14 +0530

    Leetcode-540

---
+++ single_element_in_sorted_array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int lo = 0;
+        int hi =nums.size()-1;
+
+        while(lo<hi){
+            int mid = lo+(hi-lo)/2;
+            
+            //force mid to be even 
+            if(mid%2 == 1){
+                mid--;
+            }
+
+            if(nums[mid] == nums[mid+1]){
+                lo = mid+2;
+            }
+
+            else{
+                hi = mid;
+            }
+        }
+        return nums[lo];
+    }
+};
