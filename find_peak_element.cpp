e91c789caaf9190303a0b1bb3ce21bf64881488c
UPASANA <151016634+upasanaprabhakar@users.noreply.github.com>
2026-01-24 23:08:04 +0530

    Leetcode-162

---
+++ find_peak_element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n = nums.size();
+        int lo = 0;
+        int hi = n-1;
+        while(lo<hi){
+            int mid = lo+(hi-lo)/2;
+            if(nums[mid] > nums[mid+1]){
+                hi = mid;
+            }
+            else{
+                lo = mid+1;
+            }
+        }
+        return lo;
+    }
+};
