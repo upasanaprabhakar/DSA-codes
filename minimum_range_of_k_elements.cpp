038c70a2e65d7541387df2ac4769b37e7792e6dc
UPASANA <151016634+upasanaprabhakar@users.noreply.github.com>
2026-01-25 22:51:50 +0530

    Leetcode-1984

---
+++ minimum_range_of_k_elements.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minimumDifference(vector<int>& nums, int k) {
+        int n = nums.size();
+        if (k == 1) return 0;
+
+        sort(nums.begin(), nums.end());
+
+        int ans = INT_MAX;
+
+        for (int i = 0; i + k - 1 < n; i++) {
+            ans = min(ans, nums[i + k - 1] - nums[i]);
+        }
+
+        return ans;
+    }
+};
