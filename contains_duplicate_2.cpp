73a5828df0e7d0da7d48887d6a24d5da03f76018
UPASANA <151016634+upasanaprabhakar@users.noreply.github.com>
2026-01-30 23:07:34 +0530

    Leetcode-219

---
+++ contains_duplicate_2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_set<int>st;
+
+        for(int i=0;i<n;i++){
+            if(st.count(nums[i])) return true;
+
+            st.insert(nums[i]);
+
+            if(st.size()>k){
+                st.erase(nums[i-k]);
+            }
+        }
+        return false;
+    }
+};
