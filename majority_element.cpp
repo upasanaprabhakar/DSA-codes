56ceac60516b2909af362d95f38074fb94eefb9c
UPASANA <151016634+upasanaprabhakar@users.noreply.github.com>
2026-01-30 23:33:16 +0530

    Leetcode-169

---
+++ majority_element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int,int>mp;
+        for(int x : nums){
+            mp[x]++;
+            if(mp[x] > n/2){
+                return x;
+            }
+        }
+        return -1;
+    }
+};
