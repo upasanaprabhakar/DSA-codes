9bec15e8d32f15740158837133f9c3e262ac6e6a
UPASANA <151016634+upasanaprabhakar@users.noreply.github.com>
2026-02-05 20:47:08 +0530

    Leetcode-128

---
+++ longest_common_sequence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        // insert all elements from nums into set
+        unordered_set<int> st(nums.begin(), nums.end());
+        int longest = 0;
+
+        for (int x : st) {
+            if (!st.count(x - 1)) {
+                int curr = x;
+                int length = 1;
+
+                while (st.count(curr + 1)) {
+                    curr++;
+                    length++;
+                }
+
+                longest = max(longest, length);
+            }
+        }
+        return longest;
+    }
+};
