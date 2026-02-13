1f1fffc2c83b46c4c19fee27952579a9b9fff69e
UPASANA <151016634+upasanaprabhakar@users.noreply.github.com>
2026-02-13 23:02:30 +0530

    Leetcode-1046

---
+++ last_stone_weight.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        int n = stones.size();
+        //max heap
+        priority_queue<int> pq;
+
+        for(int stone : stones){
+            pq.push(stone);
+        }
+
+        while(pq.size() > 1){
+            int x = pq.top();
+            pq.pop();
+
+            int y = pq.top();
+            pq.pop();
+
+            if(x != y){
+                pq.push(x-y);
+            }
+        }
+
+        if(pq.empty()){
+            return 0;
+        }
+
+        return pq.top();
+    }
+};
