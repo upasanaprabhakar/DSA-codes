3337d4fe957d0065fc9b91bdbbb8f6a5f8d24033
UPASANA <151016634+upasanaprabhakar@users.noreply.github.com>
2026-02-24 22:41:01 +0530

    Leetcode-39

--- combination_sum.cpp
+++ combination_sum.cpp
@@ -1,29 +1,27 @@
 class Solution {
-    public:
-        vector<vector<int> > result;
-        void f(vector<int>&cand,int t,int idx,vector<int>&subset){
-            if(t==0){
-                //found a subset
-                result.push_back(subset);
-                return;
-            }
-            if(idx==cand.size()) return;
-    
-            //pick
-            if(cand[idx]<=t){
-                subset.push_back(cand[idx]);
-                f(cand,t-cand[idx],idx,subset);
-                subset.pop_back();
-            }
-            //avoid
-            int j=idx+1;
-            f(cand,t,j,subset);
+public:
+    void helper(vector<int> & candidates, int target, vector<int>&curr, vector<vector<int>> &ans, int idx=0){
+        if(target == 0){
+            ans.push_back(curr);
+            return;
         }
-        vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-            sort(candidates.begin(),candidates.end());
-            vector<int>subset;
-            result.clear();
-            f(candidates,target,0,subset);
-            return result;
+
+        for(int i=idx;i<candidates.size();i++){
+            //skip
+            if(candidates[i] > target){
+                continue;
+            }
+
+            curr.push_back(candidates[i]);
+            //stay at the idx 
+            helper(candidates, target-candidates[i], curr, ans, i);
+            curr.pop_back();  //backtrack
         }
-    };
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> curr;
+        helper(candidates, target, curr, ans , 0);
+        return ans;
+    }
+};
