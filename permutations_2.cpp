2af7955a4db1a098c48880ebb866c90b558cd23b
UPASANA <151016634+upasanaprabhakar@users.noreply.github.com>
2026-02-25 21:57:29 +0530

    Leetcode-47

--- permutations_2.cpp
+++ permutations_2.cpp
@@ -1,23 +1,34 @@
 class Solution {
-    public:
-        void permutation(vector<vector<int>>&ans,vector<int>& nums,int i){
-            if(i==nums.size()){
-                ans.push_back(nums);
-                return;
+public:
+    void helper(vector<int>& nums, vector<vector<int>>& ans, vector<int>& curr, vector<bool>& used){
+        if(curr.size() == nums.size()){
+            ans.push_back(curr);
+            return;
+        }
+
+        for(int i=0;i<nums.size();i++){
+            if(used[i]){
+                continue;
             }
-            unordered_set<int> s;
-            for(int idx=i;idx<nums.size();idx++){
-                if(s.count(nums[idx])) continue;
-                s.insert(nums[idx]);
-                swap(nums[idx],nums[i]);
-                permutation(ans,nums,i+1);
-                swap(nums[idx],nums[i]); //undo change
-            } 
+
+            if(i>0 && nums[i] == nums[i-1] && !used[i-1]){
+                continue;
+            }
+
+            used[i] = true;
+            curr.push_back(nums[i]);
+            helper(nums, ans, curr, used);
+
+            curr.pop_back();
+            used[i] = false;
         }
-        vector<vector<int>> permuteUnique(vector<int>& nums) {
-            sort(nums.begin(), nums.end()); // required to group duplicates
-            vector<vector<int>> ans;
-            permutation(ans, nums, 0);
-            return ans;
-        }
-    };
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        vector<int> curr;
+        vector<bool> used(nums.size(), false);
+        helper(nums, ans, curr, used);
+        return ans;
+    }
+};
