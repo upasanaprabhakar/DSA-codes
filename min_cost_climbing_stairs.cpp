d0a62120487108c8519b10ed13ca791ae1d401fd
UPASANA <151016634+upasanaprabhakar@users.noreply.github.com>
2026-02-27 21:41:04 +0530

    Leetcode-746

--- min_cost_climbing_stairs.cpp
+++ min_cost_climbing_stairs.cpp
@@ -1,13 +1,16 @@
 class Solution {
-    public:
-        int helper(vector<int>&cost,int i,vector<int>&dp){
-            if(i==0 || i==1) return cost[i];
-            if(dp[i]!=-1) return dp[i];
-            return dp[i]=cost[i]+min(helper(cost,i-1,dp),helper(cost,i-2,dp));
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+
+        vector<int> dp(n);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        for(int i=2;i<n;i++){
+            dp[i] = cost[i] + min(dp[i-1], dp[i-2]);
         }
-        int minCostClimbingStairs(vector<int>& cost) {
-            int n=cost.size();
-            vector<int>dp(n,-1);
-            return min(helper(cost,n-1,dp),helper(cost,n-2,dp));
-        }
-    };
+
+        return min(dp[n-1], dp[n-2]);
+    }
+};
