2c90124e2ebac22e87471262af076fb304b26b20
UPASANA <151016634+upasanaprabhakar@users.noreply.github.com>
2026-02-27 21:49:20 +0530

    Leetcode-198

--- house_robber.cpp
+++ house_robber.cpp
@@ -1,16 +1,19 @@
 class Solution {
-    public:
-        vector<int> dp;
-        int helper(vector<int>arr, int i){
-            if(i==arr.size()-1) return arr[i];
-            if(i==arr.size()-2) return max(arr[i],arr[i+1]);
-            if(dp[i]!=-1) return dp[i];
-            return dp[i]=max((arr[i]+helper(arr,i+2)),(0+helper(arr,i+1)));
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+
+        if(n == 1){
+            return nums[0];
         }
-        
-        int rob(vector<int>& nums) {
-            dp.clear();
-            dp.resize(105,-1);
-            return helper(nums,0);
+
+        vector<int> dp(n);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        for(int i=2;i<n;i++){
+            dp[i] = max(dp[i-1], nums[i]+dp[i-2]);
         }
-    };
+        return dp[n-1];
+    }
+};
