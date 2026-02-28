37f32de6275dced37eaa025621d714571fb8f57f
UPASANA <151016634+upasanaprabhakar@users.noreply.github.com>
2026-02-28 16:34:54 +0530

    Leetcode-91

---
+++ decode_ways.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.size();
+        if(n == 0 || s[0] == '0'){
+            return 0;
+        }
+        vector<int> dp(n+1);
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i=2;i<=n;i++){
+            //single dgit check
+            if(s[i-1] != '0'){
+                dp[i] += dp[i-1];
+            }
+
+            //two digit check
+            int twoDigit = stoi(s.substr(i-2, 2));
+            if(twoDigit >= 10 && twoDigit <= 26){
+                dp[i] += dp[i-2];
+            }
+        }
+
+        return dp[n];
+    }
+};
