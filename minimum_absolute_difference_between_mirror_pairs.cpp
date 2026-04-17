ad22b12a5592615aaebe3b4607efe059568cbb5c
UPASANA <151016634+upasanaprabhakar@users.noreply.github.com>
2026-04-17 21:48:54 +0530

    Leetcode-3761

---
+++ minimum_absolute_difference_between_mirror_pairs.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int getReverse(int n) {
+        int rev = 0;
+
+        while(n > 0) {
+            int rem = n%10;
+
+            rev = rev*10 + rem;
+
+            n /= 10;
+        }
+
+        return rev;
+    }
+    int minMirrorPairDistance(vector<int>& nums) {
+        int n = nums.size();
+
+        unordered_map<int, int> mp; //reversed -> idx
+
+        int result = INT_MAX;
+
+        for(int i = 0; i < n; i++) {
+            if(mp.count(nums[i])) {
+                result = min(result, i - mp[nums[i]]);
+            }
+
+            mp[getReverse(nums[i])] = i;
+        }
+
+        return result == INT_MAX ? -1 : result;
+    }
+};
+
