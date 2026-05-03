90a5dd43388a794f48e14a7f3bc13616d87a7659
UPASANA <151016634+upasanaprabhakar@users.noreply.github.com>
2026-05-03 20:15:09 +0530

    Leetcode-1143

---
+++ longest_common_subsequence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string s1, string s2) {
+        int n= s1.size();
+        int m= s2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1[n-1] == s2[m-1]){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]= max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
